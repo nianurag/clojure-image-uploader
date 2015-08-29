fd2db8cfdaf9f36782fa4a01d870e19c2b4a8256
Anurag Adhikary <nianurag@gmail.com>
2015-08-29 11:54:19 +0530

    Added routes and made folder structure to upload and store modified images

--- ivory/project.clj
+++ ivory/project.clj
@@ -6,11 +6,6 @@
   :dependencies [[org.clojure/clojure "1.7.0"]
                  [http-kit "2.1.18"]
                  [hiccup "1.0.5"]
-                 [io.aviso/pretty "0.1.19"]
                  [metosin/compojure-api "0.22.2"]]
-  :plugins [[lein-kibit "0.1.2"]]
-  :repl-options {
-                 :nrepl-middleware [io.aviso.nrepl/pretty-middleware]
-                 }
   :main ivory.core
   )
--- ivory/src/ivory/core.clj
+++ ivory/src/ivory/core.clj
@@ -1,10 +1,32 @@
 (ns ivory.core
-  (:require [org.httpkit.server :as server]))
+  (:require [compojure.route :as route :refer [files not-found]]
+            [compojure.handler :refer [site]] ; form, query params decode; cookie; session, etc
+            [compojure.core :refer [defroutes GET POST DELETE ANY context]]
+            [org.httpkit.server :refer [run-server]]
+            [hiccup.core :as hiccup :refer [html]]))
 
-(defn app [req]
+(defn show-landing-page [req]
+  {:status  200
+   :headers {"Content-Type" "text/html"}
+   :body    "<h1>Landing Page</h2>"})
+
+(defn hello-name [name]
+  (hiccup/html
+    [:html
+     [:body
+      [:h1 {:class "title"}
+       (str "Hello " name)]]]))
+
+(defn status [req]
   {:status  200
    :headers {"Content-Type" "text/html"}
    :body    "Server is working fine"})
 
+(defroutes all-routes
+  (GET "/" [] show-landing-page)
+  (GET "/hello/:name" [name] hello-name)
+  (route/resources "/")
+  (route/not-found "<p>404, Page not found.</p>")) ;; all other, return 404
+
 (defn -main []
-  (server/run-server app {:port 8080}))
+  (run-server (site #'all-routes) {:port 8080}))
